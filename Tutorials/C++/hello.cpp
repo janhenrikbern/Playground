551648fec7d61523584af345db5fa6213a62bd02
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-06-06 10:50:23 -0400

    Adding some C++

---
+++ Tutorials/C++/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+float n;
+
+int main () {
+	std::cout << "Hello World\n";
+	std::cout << "Enter a number...\n";
+	std::cin >> n;
+	std::cout << "The square of the number is " << n * n << "\n";
+	return (0);
+}
+
